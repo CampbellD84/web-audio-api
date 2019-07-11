ef41e607f6ecf6612e2070f99d95e32b553560cc
Douglas Campbell <dacampbelltbone@gmail.com>
2019-07-10 22:41:55 -0400

    Testing React with Web Audio API; Got it running

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import AudioAnalyser from "./components/AudioAnalyser";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: null
+    };
+    this.toggleMic = this.toggleMic.bind(this);
+  }
+
+  async getMic() {
+    const audio = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false
+    });
+    this.setState({ audio });
+  }
+
+  stopMic() {
+    this.state.audio.getTracks().forEach(trk => trk.stop());
+    this.setState({ audio: null });
+  }
+
+  toggleMic() {
+    if (this.state.audio) {
+      this.stopMic();
+    } else {
+      this.getMic();
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <main>
+          <div className="controls">
+            <button onClick={this.toggleMic}>
+              {this.state.audio ? "Stop Mic" : "Start Mic"}
+            </button>
+          </div>
+          {this.state.audio ? <AudioAnalyser audio={this.state.audio} /> : ""}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
+
